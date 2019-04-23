50be8670083c5674a6a8ec6bc03dece0b66df56f
xgfone <xgfone@126.com>
2019-04-23 15:33:12 +0800

    print the type of the bool flag when outputting the usage

--- parser_flag_print_usage_10.go
+++ parser_flag_print_usage_10.go
@@ -19,9 +19,6 @@ package gconf
 import (
 	"flag"
 	"fmt"
-	"io"
-	"reflect"
-	"strings"
 )
 
 func printDefaultFlagUsage(fset *flag.FlagSet) {
@@ -31,47 +28,4 @@ func printDefaultFlagUsage(fset *flag.FlagSet) {
 		fmt.Fprintf(fset.Output(), "Usage of %s:\n", fset.Name())
 	}
 	PrintFlagUsage(fset.Output(), fset, false)
-}
-
-// PrintFlagUsage prints the usage of flag.FlagSet, which is almost equal to
-// flag.FlagSet.PrintDefaults(), but print the double prefixes "--"
-// for the long name of the option.
-func PrintFlagUsage(w io.Writer, fset *flag.FlagSet, exceptDefault bool) {
-	fset.VisitAll(func(_flag *flag.Flag) {
-		// Two spaces before -; see next two comments.
-		prefix := "  -"
-		if len(_flag.Name) > 1 {
-			prefix += "-"
-		}
-
-		s := fmt.Sprintf(prefix+"%s", _flag.Name)
-		name, usage := flag.UnquoteUsage(_flag)
-		if len(name) > 0 {
-			s += " " + name
-		}
-		// Boolean flags of one ASCII letter are so common we
-		// treat them specially, putting their usage on the same line.
-		if len(s) <= 4 { // space, space, '-', 'x'.
-			s += "\t"
-		} else {
-			// Four spaces before the tab triggers good alignment
-			// for both 4- and 8-space tab stops.
-			s += "\n    \t"
-		}
-		s += strings.Replace(usage, "\n", "\n    \t", -1)
-
-		if !exceptDefault || !isZeroValue(_flag, _flag.DefValue) {
-			vf := reflect.ValueOf(_flag.Value)
-			if vf.Kind() == reflect.Ptr {
-				vf = vf.Elem()
-			}
-			if vf.Kind() == reflect.String {
-				// put quotes on the value
-				s += fmt.Sprintf(" (default %q)", _flag.DefValue)
-			} else {
-				s += fmt.Sprintf(" (default %s)", _flag.DefValue)
-			}
-		}
-		fmt.Fprint(w, s, "\n")
-	})
 }
